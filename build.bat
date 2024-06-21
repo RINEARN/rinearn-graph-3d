64056c62f332509e58e62cbfcbafdd5b0ded160c
RINEARN <commit@rinearn.com>
2024-06-21 18:15:09 +0900

    Fix: build scripts: had not specified interfaces to be compiled explicitly
    For details, see the following commit of the Vnano Engine's repository:
    
    * "Fix: build scripts: compile plug-in interfaces explicitly" (Jun 21, 2024)
    * commit ID: 41549a65fab964743cf1d4a87acc955e91cb14b2

--- build.bat
+++ build.bat
@@ -9,7 +9,8 @@
 cd lib\app-dependencies\vnano-engine
 mkdir bin
 cd src
-javac -d ../bin -encoding UTF-8  @org/vcssl/nano/sourcelist.txt
+javac -d ../bin -encoding UTF-8 @org/vcssl/connect/sourcelist.txt
+javac -d ../bin -encoding UTF-8 @org/vcssl/nano/sourcelist.txt
 cd ..
 jar cvfm Vnano.jar src/org/vcssl/nano/meta/main.mf -C bin org -C src/org/vcssl/nano/meta META-INF
 cd ..\..\..\
