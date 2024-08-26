17728de6b08549ff06f250159258799c6d0b968b
RINEARN <commit@rinearn.com>
2024-08-27 00:37:11 +0900

    Add: DataFileHandler: API listeners of openDataFile(...), openDataFiles(...)

--- src/com/rinearn/graph3d/def/ErrorMessage.java
+++ src/com/rinearn/graph3d/def/ErrorMessage.java
@@ -26,6 +26,18 @@ public final class ErrorMessage {
 	 * Generates the error message corresponding with the specified error type, in the language of the specified locale.
 	 *
 	 * @param errorType The type of the error.
+	 * @param word The word to be embedded in the error message.
+	 * @return The generated error message.
+	 */
+	public static String generateErrorMessage(ErrorType errorType, String word) {
+		return generateErrorMessage(errorType, new String[] {word});
+	}
+
+
+	/**
+	 * Generates the error message corresponding with the specified error type, in the language of the specified locale.
+	 *
+	 * @param errorType The type of the error.
 	 * @param words The words to be embedded in the error message.
 	 * @return The generated error message.
 	 */
@@ -55,6 +67,7 @@ public final class ErrorMessage {
 			case UNSUPPORTED_DATA_FILE_FORMAT_FOR_THIS_DECODER : return "このデコーダは、指定されたデータファイル書式「" + words[0] + "」に対応していません。";
 			case INCORREDCT_NUMBER_OF_COLUMNS_IN_DATA_FILE : return "このデータの列数は " + words[0] + " であるべきですが、" + words[1] + " 列の行が含まれています。（データファイル書式: " + words[2] + "）";
 			case FAILED_TO_PARSE_NUMBER_IN_DATA_FILE : return "データ内に記述されている値「" + words[0] + "」を、数値として解釈できませんでした。";
+			case FAILED_TO_LOAD_DATA_FILE: return "データファイル「" + words[0] + "」の読み込みに失敗しました。";
 			default : return "不明なエラー種類：" + errorType;
 		}
 	}
@@ -73,6 +86,7 @@ public final class ErrorMessage {
 			case UNSUPPORTED_DATA_FILE_FORMAT_FOR_THIS_DECODER : return "This decoder does not support the specified data file format: " + words[0] + ".";
 			case INCORREDCT_NUMBER_OF_COLUMNS_IN_DATA_FILE : return "The number of the columns in this data is expected to be \"" + words[0] + "\", but a line(s) having " + words[1] + "-columns is contained. (The data file format: " + words[2] + ")";
 			case FAILED_TO_PARSE_NUMBER_IN_DATA_FILE : return "Failed to parse the value \"" + words[0] + "\", which is described in the data, as a number.";
+			case FAILED_TO_LOAD_DATA_FILE: return "Failed to load the data file \"" + words[0] + "\".";
 			default : return "Unknown Error Type: " + errorType;
 		}
 	}
--- src/com/rinearn/graph3d/presenter/handler/DataFileHandler.java
+++ src/com/rinearn/graph3d/presenter/handler/DataFileHandler.java
@@ -4,6 +4,21 @@ import com.rinearn.graph3d.model.Model;
 import com.rinearn.graph3d.presenter.Presenter;
 import com.rinearn.graph3d.view.View;
 
+import com.rinearn.graph3d.RinearnGraph3DDataFileFormat;
+import com.rinearn.graph3d.model.io.DataFileIO;
+import com.rinearn.graph3d.model.io.DataFileFormatException;
+import com.rinearn.graph3d.model.dataseries.DataSeriesGroup;
+import com.rinearn.graph3d.model.dataseries.ArrayDataSeries;
+import com.rinearn.graph3d.def.ErrorType;
+import com.rinearn.graph3d.def.ErrorMessage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.SwingUtilities;
+
+
 /**
  * The class handling events and API requests for plotting data files.
  */
@@ -54,6 +69,254 @@ public final class DataFileHandler {
 	 */
 	public synchronized boolean isEventHandlingEnabled() {
 		return this.eventHandlingEnabled;
+	}
+
+
+	// ================================================================================
+	//
+	// - API Listeners -
+	//
+	// ================================================================================
+
+
+	/**
+	 * Opens and plots the data file, by inferring the data file format from the file content.
+	 *
+	 * @param file The data file to be plotted.
+	 * @throw IOException Thrown if it failed to load the data file, due to I/O errors, syntax errors, etc.
+	 */
+	public void openDataFile(File file) throws IOException {
+
+		// Handle the API request on the event-dispatcher thread.
+		DataFileAPIListener apiListener = new DataFileAPIListener(file);
+		if (SwingUtilities.isEventDispatchThread()) {
+			apiListener.run();
+		} else {
+			try {
+				SwingUtilities.invokeAndWait(apiListener);
+			} catch (InvocationTargetException | InterruptedException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+
+		// If any error has occurred while loading the file, re-throw it to the caller side.
+		if (apiListener.hasOccurredException()) {
+			throw apiListener.getOccurredException();
+		}
+	}
+
+
+	/**
+	 * Opens and plots the data file.
+	 *
+	 * @param file The data file to be plotted.
+	 * @param format The format of the data file.
+	 * @throw IOException Thrown if it failed to load the data file, due to I/O errors, syntax errors, etc.
+	 */
+	public void openDataFile(File file, RinearnGraph3DDataFileFormat format) throws IOException {
+
+		// Handle the API request on the event-dispatcher thread.
+		DataFileAPIListener apiListener = new DataFileAPIListener(file, format);
+		if (SwingUtilities.isEventDispatchThread()) {
+			apiListener.run();
+		} else {
+			try {
+				SwingUtilities.invokeAndWait(apiListener);
+			} catch (InvocationTargetException | InterruptedException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+
+		// If any error has occurred while loading the file, re-throw it to the caller side.
+		if (apiListener.hasOccurredException()) {
+			throw apiListener.getOccurredException();
+		}
+	}
+
+
+	/**
+	 * Opens and plots the multiple data files, by inferring the data file format from the file contents.
+	 *
+	 * @param file The data files to be plotted.
+	 * @throw IOException Thrown if it failed to load the data files, due to I/O errors, syntax errors, etc.
+	 */
+	public void openDataFile(File[] files) throws IOException {
+
+		// Handle the API request on the event-dispatcher thread.
+		DataFileAPIListener apiListener = new DataFileAPIListener(files);
+		if (SwingUtilities.isEventDispatchThread()) {
+			apiListener.run();
+		} else {
+			try {
+				SwingUtilities.invokeAndWait(apiListener);
+			} catch (InvocationTargetException | InterruptedException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+
+		// If any error has occurred while loading the file, re-throw it to the caller side.
+		if (apiListener.hasOccurredException()) {
+			throw apiListener.getOccurredException();
+		}
+	}
+
+
+	/**
+	 * Opens and plots the multiple data files.
+	 *
+	 * @param file The data files to be plotted.
+	 * @param format The formats of the data files.
+	 * @throw IOException Thrown if it failed to load the data files, due to I/O errors, syntax errors, etc.
+	 */
+	public void openDataFile(File[] files, RinearnGraph3DDataFileFormat[] formats) throws IOException {
+
+		// Handle the API request on the event-dispatcher thread.
+		DataFileAPIListener apiListener = new DataFileAPIListener(files, formats);
+		if (SwingUtilities.isEventDispatchThread()) {
+			apiListener.run();
+		} else {
+			try {
+				SwingUtilities.invokeAndWait(apiListener);
+			} catch (InvocationTargetException | InterruptedException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+
+		// If any error has occurred while loading the file, re-throw it to the caller side.
+		if (apiListener.hasOccurredException()) {
+			throw apiListener.getOccurredException();
+		}
+	}
+
+
+	/**
+	 * The class handling API requests from openDataFile(...) and openDataFiles(...) methods,
+	 * on event-dispatcher thread.
+	 */
+	private final class DataFileAPIListener implements Runnable {
+
+		/** The data file to be plotted. */
+		private final File[] dataFiles;
+
+		/** The format of the data file. */
+		private final RinearnGraph3DDataFileFormat[] formats;
+
+		/** The exception which was occurred when the data was loaded last time. */
+		private volatile IOException occurredException = null;
+
+		/**
+		 * Creates a new instance handling openDataFile(File) method
+		 * on event-dispatcher thread.
+		 *
+		 * @param dataFile The data file to be plotted.
+		 */
+		public DataFileAPIListener(File dataFile) {
+			this(new File[] {dataFile});
+		}
+
+		/**
+		 * Creates a new instance handling openDataFile(File, RinearnGraph3DDataFileFormat) method
+		 * on event-dispatcher thread.
+		 *
+		 * @param dataFile The data file to be plotted.
+		 * @param format The format of the data file.
+		 */
+		public DataFileAPIListener(File dataFile, RinearnGraph3DDataFileFormat format) {
+			this(new File[] {dataFile}, new RinearnGraph3DDataFileFormat[] {format});
+		}
+
+		/**
+		 * Creates a new instance handling openDataFiles(File[]) method
+		 * on event-dispatcher thread.
+		 *
+		 * @param dataFiles The data files to be plotted.
+		 */
+		public DataFileAPIListener(File[] dataFiles) {
+			int dataFileCount = dataFiles.length;
+			this.dataFiles = dataFiles;
+			this.formats = new RinearnGraph3DDataFileFormat[dataFileCount];
+			Arrays.fill(formats, RinearnGraph3DDataFileFormat.AUTO);
+		}
+
+		/**
+		 * Creates a new instance handling openDataFiles(File[], RinearnGraph3DDataFileFormat[]) method
+		 * on event-dispatcher thread.
+		 *
+		 * @param dataFiles The data files to be plotted.
+		 * @param formats The formats of the data file.
+		 */
+		public DataFileAPIListener(File[] dataFiles, RinearnGraph3DDataFileFormat[] formats) {
+			if (dataFiles.length != formats.length) {
+				throw new IllegalArgumentException("The lengths of dataFiles[] and formats[] must be the same.");
+			}
+			this.dataFiles = dataFiles;
+			this.formats = formats;
+		}
+
+		/**
+		 * Checks whether any exception occurred when loading the data last time.
+		 *
+		 * @return Return true if any exception occurred.
+		 */
+		public synchronized boolean hasOccurredException() {
+			return this.occurredException != null;
+		}
+
+		/**
+		 * Gets the exception which occurred when loading the data last time.
+		 *
+		 * @return Return the exception which occurred when loading the data last time.
+		 */
+		public synchronized IOException getOccurredException() {
+			return this.occurredException;
+		}
+
+		/**
+		 * Loads the data file.
+		 */
+		@Override
+		public synchronized void run() {
+			DataFileIO dataFileIO = new DataFileIO();
+			int dataFileCount = this.dataFiles.length;
+
+			// Clear the error info.
+			this.occurredException = null;
+
+			// Stores the all data series loaded from the multiple data files into the following data series group.
+			DataSeriesGroup<ArrayDataSeries> allDataSeriesGroup = new DataSeriesGroup<ArrayDataSeries>();
+
+			// Load the data files.
+			for (int ifile=0; ifile<dataFileCount; ifile++) {
+				try {
+
+					// Load the ifile-th data file, and store the result into "allDataSeriesGroup".
+					DataSeriesGroup<ArrayDataSeries> loadedDataFromFile = dataFileIO.loadDataFile(dataFiles[ifile], formats[ifile]);
+					for (ArrayDataSeries dataSeries: loadedDataFromFile.getDataSeriesList()) {
+						allDataSeriesGroup.addDataSeries(dataSeries);
+					}
+
+				} catch (IOException ioe) {
+					String errorMessage = ErrorMessage.generateErrorMessage(ErrorType.FAILED_TO_LOAD_DATA_FILE, this.dataFiles[ifile].getPath());
+					this.occurredException = new IOException(errorMessage, ioe);
+					return;
+
+				} catch (DataFileFormatException dffe) {
+					String errorMessage = ErrorMessage.generateErrorMessage(ErrorType.FAILED_TO_LOAD_DATA_FILE, this.dataFiles[ifile].getPath());
+					this.occurredException = new IOException(errorMessage, dffe);
+					return;
+				}
+			}
+
+			// Set the loaded data to Model.
+			model.setArrayDataSeriesGroup(allDataSeriesGroup);
+
+			// Re-plot the graph.
+			presenter.plot();
+		}
 	}
 
 }
