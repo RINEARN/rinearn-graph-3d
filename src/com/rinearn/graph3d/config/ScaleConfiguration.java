02d2532cafefe854c45c8ab5e95bf9a55ca34127
RINEARN <commit@rinearn.com>
2024-12-05 21:37:19 +0900

    Refactor: move config/ScaleConfiguration.NumericTickLabelFormatter to config/scale/NumericTickLabelFormatter

--- src/com/rinearn/graph3d/config/ScaleConfiguration.java
+++ src/com/rinearn/graph3d/config/ScaleConfiguration.java
@@ -190,7 +190,6 @@ public final class ScaleConfiguration {
 		this.colorBarScaleConfiguration.validate();
 	}
 
-
 	/**
 	 * The class storing configuration values of the scale of an axis (X, Y, or Z).
 	 */
@@ -230,6 +229,13 @@ public final class ScaleConfiguration {
 		 */
 		private volatile NumericTickLabelFormatter[] numericTickLabelFormatters = {
 
+				// !!!!!
+				// この仕様、見直した方がいいよ。塩漬けを経ていま見ると直観で分かり辛いし、思い出さないと納得できない。
+				// しかも配列順序で意味持たせてるのもやっぱり良くない。
+				// 現状のUIの通りで構造化したらいい気がする。というかあれで駄目ならMANUALモード使って割り当てればいいんだし。
+				// MANUALモードがある以上、あんまりこの自動フォーマットの仕様を汎用性のため複雑化するのも本末転倒で。
+				// ほどよいシンプルさとほどよい実用性があればそれでいい。現状のUIはそのへんよくバランスとって収束してる。
+
 				// The base format, applied to any ticks
 				// excluding if their coordinates are contained in the range of formatters defined latter.
 				new NumericTickLabelFormatter(new DecimalFormat("0.0#E0")),
@@ -237,7 +243,7 @@ public final class ScaleConfiguration {
 				// The format applied to only for the range (0.1, 10), which does not contain 0.1 and 10.
 				new NumericTickLabelFormatter(new DecimalFormat("0.0#"), new BigDecimal("0.1"), BigDecimal.TEN, false, false),
 
-				// The format applied to only for the range (-10, 0.1), which does not contain -10 and -0.1.
+				// The format applied to only for the range (-10, -0.1), which does not contain -10 and -0.1.
 				new NumericTickLabelFormatter(new DecimalFormat("0.0#"), BigDecimal.TEN.negate(), new BigDecimal("0.1").negate(), false, false),
 
 				// The format applied to only zero.
@@ -495,146 +501,6 @@ public final class ScaleConfiguration {
 
 		/** Align scale ticks manually, and also specify arbitrary tick labels. */
 		MANUAL,
-	}
-
-
-	// !!! NOTE !!!
-	//
-	// ↓これやっぱ外にあった方がいいなあ、どう考えても
-	//
-	// !!! NOTE !!!
-
-	/**
-	 * The formatter of displayed values of numeric tick labels.
-	 */
-	public static class NumericTickLabelFormatter {
-
-		/** The displayed format of numeric tick labels. */
-		private final NumberFormat format;
-		// Note: Why we don't declare the above as DecimalFormat-type field is,
-		//       some people may want to implement original format by extending NumberFormat.
-		//       (NumberFormat is an abstract class, and DecimalFormat is one of its implementation.)
-
-		/** The flag representing whether the applicable range of this formatter is defined. */
-		private final boolean hasRange;
-
-		/** The minimum value of the range to which apply this formatter. */
-		private final BigDecimal minCoordinate;
-
-		/** The maximum value of the range to which apply this formatter. */
-		private final BigDecimal maxCoordinate;
-
-		/**
-		 * The flag representing whether apply this formatter to a label
-		 * of which coordinate is just the same as minimum value of the range.
-		 */
-		private final boolean containsMinCoordinate;
-
-		/**
-		 * The flag representing whether apply this formatter to a label
-		 * of which coordinate is just the same as maximum value of the range.
-		 */
-		private final boolean containsMaxCoordinate;
-
-
-		/**
-		 * Creates a new formatter for formatting any labels of ticks.
-		 *
-		 * @param format The format of labels.
-		 */
-		public NumericTickLabelFormatter(NumberFormat format) {
-			this.format = format;
-			this.hasRange = false;
-			this.minCoordinate = null;
-			this.maxCoordinate = null;
-			this.containsMinCoordinate = false;
-			this.containsMaxCoordinate = false;
-		}
-
-		/**
-		 * Creates a new formatter for formatting only labels of ticks in the specific range.
-		 *
-		 * @param format The displayed format of numeric labels.
-		 * @param minCoordinate The minimum coordinate of the range to which apply this formatter.
-		 * @param maxCoordinate The minimum coordinate of the range to which apply this formatter.
-		 * @param containsMinCoordinate Specify true if apply this formatter to a label of which coordinate is just the same as minCoordinate.
-		 * @param containsMaxCoordinate Specify true if apply this formatter to a label of which coordinate is just the same as maxCoordinate.
-		 */
-		public NumericTickLabelFormatter(NumberFormat format,
-				BigDecimal minCoordinate, BigDecimal maxCoordinate,
-				boolean containsMinCoordinate, boolean containsMaxCoordinate) {
-
-			this.format = format;
-			this.hasRange = true;
-			this.minCoordinate = minCoordinate;
-			this.maxCoordinate = maxCoordinate;
-			this.containsMinCoordinate = containsMinCoordinate;
-			this.containsMaxCoordinate = containsMaxCoordinate;
-		}
-
-
-		/**
-		 * Formats the specified coordinate value into a label text,
-		 *
-		 * @param tickCoordinate The coordinate of the numeric tick to be formatted.
-		 * @return The formatted tick label (or the specified label as it is).
-		 */
-		public synchronized String format(BigDecimal tickCoordinate) {
-			return this.format.format(tickCoordinate);
-		}
-
-		/**
-		 * Returns whether the specified coordinate is contained in the applicable range of this formatter.
-		 * If no applicable range is defined, always returns true.
-		 *
-		 * @param coordinate Returns if the specified coordinate is contained in the range.
-		 */
-		public synchronized boolean contains(BigDecimal coordinate) {
-			if (!this.hasRange) {
-				return true;
-			}
-
-			// Firstly, compare the specified coordinate to the minimum value of the range.
-			int minCompared = coordinate.compareTo(this.minCoordinate);
-
-			// The case that we allow the completely same value as the min value.
-			if (this.containsMinCoordinate) {
-
-				// Decline if the specified coordinate is LESS THAN the min value.
-				if (minCompared < 0) {
-					return false;
-				}
-
-			} else {
-
-				// Decline if the specified coordinate is LESS THAN OR EQUALS TO the min value.
-				if (minCompared <= 0) {
-					return false;
-				}
-			}
-
-			// Next, compare the specified coordinate to the maximum value of the range.
-			int maxCompared = coordinate.compareTo(this.maxCoordinate);
-
-			// The case that we allow the completely same value as the max value.
-			if (this.containsMaxCoordinate) {
-
-				// Decline if the specified coordinate is GREATER THAN the min value.
-				if (0 < maxCompared) {
-					return false;
-				}
-
-			} else {
-
-				// Decline if the specified coordinate is GREATER THAN OR EQUALS TO the min value.
-				if (0 <= maxCompared) {
-					return false;
-				}
-			}
-
-			// If all tests have passed, the specified coordinate is contained in the range.
-			return true;
-		}
 	}
 
 }
---
+++ src/com/rinearn/graph3d/config/scale/NumericTickLabelFormatter.java
@@ -0,0 +1,143 @@
+package com.rinearn.graph3d.config.scale;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+
+/**
+ * The formatter of displayed values of numeric tick labels.
+ */
+public final class NumericTickLabelFormatter {
+
+	/** The displayed format of numeric tick labels. */
+	private final NumberFormat format;
+	// Note: Why we don't declare the above as DecimalFormat-type field is,
+	//       some people may want to implement original format by extending NumberFormat.
+	//       (NumberFormat is an abstract class, and DecimalFormat is one of its implementation.)
+
+	/** The flag representing whether the applicable range of this formatter is defined. */
+	private final boolean hasRange;
+
+	/** The minimum value of the range to which apply this formatter. */
+	private final BigDecimal minCoordinate;
+
+	/** The maximum value of the range to which apply this formatter. */
+	private final BigDecimal maxCoordinate;
+
+	/**
+	 * The flag representing whether apply this formatter to a label
+	 * of which coordinate is just the same as minimum value of the range.
+	 */
+	private final boolean containsMinCoordinate;
+
+	/**
+	 * The flag representing whether apply this formatter to a label
+	 * of which coordinate is just the same as maximum value of the range.
+	 */
+	private final boolean containsMaxCoordinate;
+
+
+	/**
+	 * Creates a new formatter for formatting any labels of ticks.
+	 *
+	 * @param format The format of labels.
+	 */
+	public NumericTickLabelFormatter(NumberFormat format) {
+		this.format = format;
+		this.hasRange = false;
+		this.minCoordinate = null;
+		this.maxCoordinate = null;
+		this.containsMinCoordinate = false;
+		this.containsMaxCoordinate = false;
+	}
+
+	/**
+	 * Creates a new formatter for formatting only labels of ticks in the specific range.
+	 *
+	 * @param format The displayed format of numeric labels.
+	 * @param minCoordinate The minimum coordinate of the range to which apply this formatter.
+	 * @param maxCoordinate The minimum coordinate of the range to which apply this formatter.
+	 * @param containsMinCoordinate Specify true if apply this formatter to a label of which coordinate is just the same as minCoordinate.
+	 * @param containsMaxCoordinate Specify true if apply this formatter to a label of which coordinate is just the same as maxCoordinate.
+	 */
+	public NumericTickLabelFormatter(NumberFormat format,
+			BigDecimal minCoordinate, BigDecimal maxCoordinate,
+			boolean containsMinCoordinate, boolean containsMaxCoordinate) {
+
+		this.format = format;
+		this.hasRange = true;
+		this.minCoordinate = minCoordinate;
+		this.maxCoordinate = maxCoordinate;
+		this.containsMinCoordinate = containsMinCoordinate;
+		this.containsMaxCoordinate = containsMaxCoordinate;
+	}
+
+
+	/**
+	 * Formats the specified coordinate value into a label text,
+	 *
+	 * @param tickCoordinate The coordinate of the numeric tick to be formatted.
+	 * @return The formatted tick label (or the specified label as it is).
+	 */
+	public synchronized String format(BigDecimal tickCoordinate) {
+		return this.format.format(tickCoordinate);
+	}
+
+	// !!!!! NOTE:
+	//   この層でこういうのが必用になるのは、管轄が切り方がおかしい。
+	//   日付とかその他文字列とか色々なフォーマット実装を考えると、
+	//   レンジ分岐が要るなら、それは複数レンジをこのクラス内で持って内部でやるべき。
+	//   もっと一般化して、特定座標がフォーマット可能かどうかを検査するなら、名前を isFormattable とかにすべき。
+	/**
+	 * Returns whether the specified coordinate is contained in the applicable range of this formatter.
+	 * If no applicable range is defined, always returns true.
+	 *
+	 * @param coordinate Returns if the specified coordinate is contained in the range.
+	 */
+	public synchronized boolean contains(BigDecimal coordinate) {
+		if (!this.hasRange) {
+			return true;
+		}
+
+		// Firstly, compare the specified coordinate to the minimum value of the range.
+		int minCompared = coordinate.compareTo(this.minCoordinate);
+
+		// The case that we allow the completely same value as the min value.
+		if (this.containsMinCoordinate) {
+
+			// Decline if the specified coordinate is LESS THAN the min value.
+			if (minCompared < 0) {
+				return false;
+			}
+
+		} else {
+
+			// Decline if the specified coordinate is LESS THAN OR EQUALS TO the min value.
+			if (minCompared <= 0) {
+				return false;
+			}
+		}
+
+		// Next, compare the specified coordinate to the maximum value of the range.
+		int maxCompared = coordinate.compareTo(this.maxCoordinate);
+
+		// The case that we allow the completely same value as the max value.
+		if (this.containsMaxCoordinate) {
+
+			// Decline if the specified coordinate is GREATER THAN the min value.
+			if (0 < maxCompared) {
+				return false;
+			}
+
+		} else {
+
+			// Decline if the specified coordinate is GREATER THAN OR EQUALS TO the min value.
+			if (0 <= maxCompared) {
+				return false;
+			}
+		}
+
+		// If all tests have passed, the specified coordinate is contained in the range.
+		return true;
+	}
+}
