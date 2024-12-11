143f3adcafa76412a7550368e5aef95af91fa736
RINEARN <commit@rinearn.com>
2024-12-11 23:36:07 +0900

    Add: FrameConfig: comment

--- src/com/rinearn/graph3d/config/FrameConfiguration.java
+++ src/com/rinearn/graph3d/config/FrameConfiguration.java
@@ -1,12 +1,17 @@
 package com.rinearn.graph3d.config;
 
+// !!!!! NOTE !!!!!
+// この設定は ScaleConfig にマージすべきか？ あえて分けるほどの規模でもないし、2Dだとどう分けるのって問題になるし。
+// 実質的にこれのメインは底面の画像設定くらいな気がする。
+//
+// -> それじゃあ、画像設定を入れてみて、規模感が小さかったらマージを検討、結構大きかったらそのまま現状で、って感じで。
+//    後でまた要検討
+
 
 /**
  * The class for storing configuration parameters of the graph frame.
  */
 public final class FrameConfiguration {
-
-	// To be added: grid-lines, images, etc. -> grid-lines は描かれ方が ticks に依存するし、ScaleConfig への依存性が生じてしまうのでこっちにあるべきではない。あっちにあるべき。
 
 	/**
 	 * The enum representing each mode of the graph frame.
