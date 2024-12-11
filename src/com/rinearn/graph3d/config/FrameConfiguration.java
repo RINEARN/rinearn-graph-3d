45ad70118f372aa588dd39499d00d660feaec5c8
RINEARN <commit@rinearn.com>
2024-12-12 02:32:18 +0900

    Add: FrameConfig: each plane's configs

--- src/com/rinearn/graph3d/config/FrameConfiguration.java
+++ src/com/rinearn/graph3d/config/FrameConfiguration.java
@@ -1,17 +1,29 @@
 package com.rinearn.graph3d.config;
 
+import java.io.File;
+import java.awt.Image;
+
+
 // !!!!! NOTE !!!!!
 // この設定は ScaleConfig にマージすべきか？ あえて分けるほどの規模でもないし、2Dだとどう分けるのって問題になるし。
 // 実質的にこれのメインは底面の画像設定くらいな気がする。
 //
 // -> それじゃあ、画像設定を入れてみて、規模感が小さかったらマージを検討、結構大きかったらそのまま現状で、って感じで。
 //    後でまた要検討
-
+//
+// -> 実装直後の判断、とりあえずやっぱり現状維持が良さそうかな。理由は以下：
+//
+//    * XY面とかのサブ要素コンテナクラス、命名的に Scale じゃ微妙で Frame の方がしっくりくる
+//    * 将来的に、床面に等高線とかヒートマップとか動的描画内容とかを描きたい場合が生じた際、それらはもはや全く Scale ではないので命名が詰む。
 
 /**
  * The class for storing configuration parameters of the graph frame.
  */
 public final class FrameConfiguration {
+
+	// !!!!! NOTE
+	// FrameMode ってのはちょっと粒度が大きすぎるかも。FrameConfiguration っていう設定単位がある前提だと。FrameShapeMode とかの方がいいかな？
+	// -> むしろ FrameConfiguration 内なのでもう Frame は省略して ShapeMode でいい。カメラアングルも CameraConfiguration.AngleMode なので。
 
 	/**
 	 * The enum representing each mode of the graph frame.
@@ -42,6 +54,25 @@ public final class FrameConfiguration {
 	/** Stores the mode of the graph frame. */
 	private volatile FrameMode frameMode = FrameMode.BOX;
 
+	/** The configuration of the lower (Z-min) side X-Y plane's frame. */
+	private volatile PlaneFrameConfiguration xyLowerFrameConfiguration = new PlaneFrameConfiguration();
+
+	/** The configuration of the upper (Z-max) side X-Y plane's frame. */
+	private volatile PlaneFrameConfiguration xyUpperFrameConfiguration = new PlaneFrameConfiguration();
+
+	/** The configuration of the lower (X-min) side Y-Z plane's frame. */
+	private volatile PlaneFrameConfiguration yzLowerFrameConfiguration = new PlaneFrameConfiguration();
+
+	/** The configuration of the upper (X-max) side Y-Z plane's frame. */
+	private volatile PlaneFrameConfiguration yzUpperFrameConfiguration = new PlaneFrameConfiguration();
+
+	/** The configuration of the lower (Y-min) side Z-X plane's frame. */
+	private volatile PlaneFrameConfiguration zxLowerFrameConfiguration = new PlaneFrameConfiguration();
+
+	/** The configuration of the upper (Y-max) side Z-X plane's frame. */
+	private volatile PlaneFrameConfiguration zxUpperFrameConfiguration = new PlaneFrameConfiguration();
+
+
 	/**
 	 * Sets the mode of the graph frame.
 	 *
@@ -59,4 +90,258 @@ public final class FrameConfiguration {
 	public synchronized FrameMode getFrameMode() {
 		return this.frameMode;
 	}
+
+
+	/**
+	 * Sets the configuration of the lower (Z-min) side X-Y plane's frame.
+	 *
+	 * @param xyLowerFrameConfiguration The configuration of the lower (Z-min) side X-Y plane's frame.
+	 */
+	public synchronized void setXyLowerFrameConfiguration(PlaneFrameConfiguration xyLowerFrameConfiguration) {
+		this.xyLowerFrameConfiguration = xyLowerFrameConfiguration;
+	}
+
+	/**
+	 * Gets the configuration of the lower (Z-min) side X-Y plane's frame.
+	 *
+	 * @return The configuration of the lower (Z-min) side X-Y plane's frame.
+	 */
+	public synchronized PlaneFrameConfiguration getXyLowerFrameConfiguration() {
+		return this.xyLowerFrameConfiguration;
+	}
+
+
+	/**
+	 * Sets the configuration of the upper (Z-min) side X-Y plane's frame.
+	 *
+	 * @param xyUpperFrameConfiguration The configuration of the upper (Z-min) side X-Y plane's frame.
+	 */
+	public synchronized void setXyUpperFrameConfiguration(PlaneFrameConfiguration xyUpperFrameConfiguration) {
+		this.xyUpperFrameConfiguration = xyUpperFrameConfiguration;
+	}
+
+	/**
+	 * Gets the configuration of the upper (Z-max) side X-Y plane's frame.
+	 *
+	 * @return The configuration of the upper (Z-max) side X-Y plane's frame.
+	 */
+	public synchronized PlaneFrameConfiguration getXyUpperFrameConfiguration() {
+		return this.xyUpperFrameConfiguration;
+	}
+
+
+	/**
+	 * Sets the configuration of the lower (X-min) side Y-Z plane's frame.
+	 *
+	 * @param yzLowerFrameConfiguration The configuration of the lower (X-min) side Y-Z plane's frame.
+	 */
+	public synchronized void setYzLowerFrameConfiguration(PlaneFrameConfiguration yzLowerFrameConfiguration) {
+		this.yzLowerFrameConfiguration = yzLowerFrameConfiguration;
+	}
+
+	/**
+	 * Gets the configuration of the lower (X-min) side Y-Z plane's frame.
+	 *
+	 * @return The configuration of the lower (X-min) side Y-Z plane's frame.
+	 */
+	public synchronized PlaneFrameConfiguration getYzLowerFrameConfiguration() {
+		return this.yzLowerFrameConfiguration;
+	}
+
+
+	/**
+	 * Sets the configuration of the upper (X-min) side Y-Z plane's frame.
+	 *
+	 * @param yzUpperFrameConfiguration The configuration of the upper (X-min) side Y-Z plane's frame.
+	 */
+	public synchronized void setYzUpperFrameConfiguration(PlaneFrameConfiguration yzUpperFrameConfiguration) {
+		this.yzUpperFrameConfiguration = yzUpperFrameConfiguration;
+	}
+
+	/**
+	 * Gets the configuration of the upper (X-max) side Y-Z plane's frame.
+	 *
+	 * @return The configuration of the upper (X-max) side Y-Z plane's frame.
+	 */
+	public synchronized PlaneFrameConfiguration getYzUpperFrameConfiguration() {
+		return this.yzUpperFrameConfiguration;
+	}
+
+
+	/**
+	 * Sets the configuration of the lower (Y-min) side Z-X plane's frame.
+	 *
+	 * @param zxLowerFrameConfiguration The configuration of the lower (Y-min) side Z-X plane's frame.
+	 */
+	public synchronized void setZxLowerFrameConfiguration(PlaneFrameConfiguration zxLowerFrameConfiguration) {
+		this.zxLowerFrameConfiguration = zxLowerFrameConfiguration;
+	}
+
+	/**
+	 * Gets the configuration of the lower (Y-min) side Z-X plane's frame.
+	 *
+	 * @return The configuration of the lower (Y-min) side Z-X plane's frame.
+	 */
+	public synchronized PlaneFrameConfiguration getZxLowerFrameConfiguration() {
+		return this.zxLowerFrameConfiguration;
+	}
+
+
+	/**
+	 * Sets the configuration of the upper (Y-min) side Z-X plane's frame.
+	 *
+	 * @param zxUpperFrameConfiguration The configuration of the upper (Y-min) side Z-X plane's frame.
+	 */
+	public synchronized void setZxUpperFrameConfiguration(PlaneFrameConfiguration zxUpperFrameConfiguration) {
+		this.zxUpperFrameConfiguration = zxUpperFrameConfiguration;
+	}
+
+	/**
+	 * Gets the configuration of the upper (Y-max) side Z-X plane's frame.
+	 *
+	 * @return The configuration of the upper (Y-max) side Z-X plane's frame.
+	 */
+	public synchronized PlaneFrameConfiguration getZxUpperFrameConfiguration() {
+		return this.zxUpperFrameConfiguration;
+	}
+
+
+	/**
+	 * The enum representing each image displaying mode on the frame of a plane.
+	 */
+	public enum ImageMode {
+
+		/** Represents the mode displaying no image. */
+		NONE,
+
+		/** Represents the mode displaying the image loaded from a file. */
+		FILE,
+
+		// SUBPLOT, // Maybe supported in future.
+
+		/** Represents the mode displaying the dynamically generated image passed through API, etc. */
+		CUSTOM,
+	}
+
+
+	/**
+	 * The class storing configuration values of a plane.
+	 */
+	public class PlaneFrameConfiguration {
+
+		/** The image displaying mode on this plane's frame. */
+		private ImageMode imageMode = ImageMode.NONE;
+
+		/** The file from which the image to be displayed on this plane's frame is loaded. */
+		private volatile File imageFile = null; // The loaded image will be drawn to the buffer in Renderer, so no Image instance here.
+
+		// private volatile int subplotIndex; // Maybe supported in future. Probably we should have subplot index here, not Image instance.
+
+		/** The custom image to be displayed on this plane's frame. */
+		private volatile Image customImage = null;
+
+		/** The width (pixel count) of the rasterized image rendered on the plane's frame. */
+		private volatile int imageResolutionConversionWidth = 140;
+		// private volatile int imageRasterizationWidth;
+
+		/** The height (pixel count) of the rasterized image rendered on the plane's frame. */
+		private volatile int imageResolutionConversionHeight = 140;
+		// private volatile int imageRasterizationHeight;
+
+
+		/**
+		 * Sets the image displaying mode on this plane's frame.
+		 *
+		 * @param imageMode The image displaying mode on this plane's frame.
+		 */
+		public synchronized void setImageMode(ImageMode imageMode) {
+			this.imageMode = imageMode;
+		}
+
+		/**
+		 * Gets the image displaying mode on this plane's frame.
+		 *
+		 * @return The image displaying mode on this plane's frame.
+		 */
+		public synchronized ImageMode getImageMode() {
+			return this.imageMode;
+		}
+
+
+		/**
+		 * Sets the file from which the image to be displayed on this plane's frame is loaded, in FILE mode.
+		 *
+		 * @param imageFile The file of the image to be displayed on this plane's frame.
+		 */
+		public synchronized void setImageFile(File imageFile) {
+			this.imageFile = imageFile;
+		}
+
+		/**
+		 * Gets the file from which the image to be displayed on this plane's frame is loaded in FILE mode.
+		 *
+		 * @return The file of the image to be displayed on this plane's frame.
+		 */
+		public synchronized File getImageFile() {
+			return this.imageFile;
+		}
+
+
+		/**
+		 * Sets the image to be displayed on this plane's frame, in CUSTOM mode.
+		 *
+		 * @param customImage The image to be displayed on this plane's frame.
+		 */
+		public synchronized void setCustomImage(Image customImage) {
+			this.customImage = customImage;
+		}
+
+		/**
+		 * Gets the image to be displayed on this plane's frame, in CUSTOM mode.
+		 *
+		 * @return The image to be displayed on this plane's frame.
+		 */
+		public synchronized Image getCustomImage() {
+			return this.customImage;
+		}
+
+
+		/**
+		 * Sets the with (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 *
+		 * @param width The width (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 */
+		public synchronized void setImageRasolutionConversionWidth(int width) {
+			this.imageResolutionConversionWidth = width;
+		}
+
+		/**
+		 * Gets the with (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 *
+		 * @return The width (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 */
+		public synchronized int getImageRasolutionConversionWidth() {
+			return this.imageResolutionConversionWidth;
+		}
+
+
+		/**
+		 * Sets the height (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 *
+		 * @param width The height (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 */
+		public synchronized void setImageRasolutionConversionHeight(int height) {
+			this.imageResolutionConversionHeight = height;
+		}
+
+		/**
+		 * Gets the height (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 *
+		 * @return The height (pixel count) of the re-rasterized image rendered on the plane's frame.
+		 */
+		public synchronized int getImageRasolutionConversionHeight() {
+			return this.imageResolutionConversionHeight;
+		}
+	}
+
 }
