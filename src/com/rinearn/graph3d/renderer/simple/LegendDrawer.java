5a9f9cd0d65017afc52080f5020846131ca75b92
RINEARN <commit@rinearn.com>
2025-02-12 15:48:58 +0900

    Refactor: LegendDrawer: separate position-calculation into methods

--- src/com/rinearn/graph3d/renderer/simple/LegendDrawer.java
+++ src/com/rinearn/graph3d/renderer/simple/LegendDrawer.java
@@ -10,6 +10,7 @@ import com.rinearn.graph3d.config.ColorConfiguration;
 import com.rinearn.graph3d.config.FontConfiguration;
 import com.rinearn.graph3d.config.LabelConfiguration;
 import com.rinearn.graph3d.config.RinearnGraph3DConfiguration;
+import com.rinearn.graph3d.config.color.GradientColor;
 
 /**
  * The class to draw legends over the 3D-rendered graph screen.
@@ -41,53 +42,40 @@ public final class LegendDrawer {
 	 * @param graphics The Graphics2D object to draw contents on the graph screen image.
 	 */
 	public void draw(Graphics2D graphics) {
-		FontConfiguration fontConfig = this.config.getFontConfiguration();
-		ColorConfiguration colorConfig = this.config.getColorConfiguration();
+
+		// Extract/define position parameters.
 		CameraConfiguration cameraConfig = this.config.getCameraConfiguration();
-		LabelConfiguration labelConfig = this.config.getLabelConfiguration();
-		LabelConfiguration.LegendLabelConfiguration legendConfig = labelConfig.getLegendLabelConfiguration();
-
-		String[] legendTexts = legendConfig.getLabelTexts();
-
-		// Set the color and font to render legends.
-		Color forgroundColor = colorConfig.getForegroundColor();
-		Font legendFont = fontConfig.getLegendLabelFont();
-
 		int screenWidth = cameraConfig.getScreenWidth();
 		int screenHeight = cameraConfig.getScreenHeight();
 		int legendOffsetX = 30;
 		int legendOffsetY = -30;
 
+		// Calculate the position of legends.
+		int[] legendTextAreaPosition = this.calculateLegendTextAraaPosition(graphics, screenWidth, screenHeight, legendOffsetX, legendOffsetY);
+		int legendTextLineHeight = this.calculateLegendTextLineHeight(graphics);
+
 		// Draw legend texts.
-		this.drawLegendTexts(
-				graphics, legendTexts,
-				screenWidth, screenHeight, legendOffsetX, legendOffsetY,
-				legendFont, forgroundColor
-		);
+		this.drawLegendTexts(graphics, legendTextAreaPosition, legendTextLineHeight);
 	}
 
+
 	/**
-	 * Draws legend texts.
+	 * Calculates the position of the left-top point of the text area of legends (not including markers).
 	 *
 	 * @param graphics The Graphics2D object to draw contents on the graph screen image.
-	 * @param legendTexts The texts of the legends.
 	 * @param screenWidth The width of the graph screen.
 	 * @param screenHeight The height of the graph screen.
 	 * @param legendOffsetX The offset amount [pixels] of the X-position of the legends.
 	 * @param legendOffsetY The offset amount [pixels] of the Y-position of the legends.
-	 * @param legendFont The font to draw the legends.
-	 * @param forgroundColor The color of the legends.
+	 * @return The array storing x (at [0]) and y (at [1]) of the left-top point of the legend text area (not including markers).
 	 */
-	private void drawLegendTexts(Graphics2D graphics, String[] legendTexts,
-			int screenWidth, int screenHeight, int legendOffsetX, int legendOffsetY,
-			Font legendFont, Color forgroundColor) {
+	private int[] calculateLegendTextAraaPosition(Graphics2D graphics, int screenWidth, int screenHeight, int legendOffsetX, int legendOffsetY) {
 
-		graphics.setColor(forgroundColor);
-		graphics.setFont(legendFont);
+		// Extract legend settings.
+		String[] legendTexts = this.config.getLabelConfiguration().getLegendLabelConfiguration().getLabelTexts();
+		Font legendFont = this.config.getFontConfiguration().getLegendLabelFont();
 
-		// Get font information.
 		FontMetrics fontMetrics = graphics.getFontMetrics(legendFont);
-		int fontHeight = fontMetrics.getAscent();
 
 		// Detect the maximum length (pixels) of the legend text on the display.
 		int maxTextWidth = 0;
@@ -98,11 +86,50 @@ public final class LegendDrawer {
 			}
 		}
 
+		int legendAreaX = screenWidth - maxTextWidth - legendOffsetX;
+		int legendAreaY = legendOffsetY;
+		return new int[] { legendAreaX, legendAreaY };
+	}
+
+
+	/**
+	 * Calculates the line-height of the legend texts.
+	 *
+	 * @param graphics The Graphics2D object to draw contents on the graph screen image.
+	 * @return The line-height of the legend texts.
+	 */
+	private int calculateLegendTextLineHeight(Graphics2D graphics) {
+		Font legendFont = this.config.getFontConfiguration().getLegendLabelFont();
+		FontMetrics fontMetrics = graphics.getFontMetrics(legendFont);
+		int fontHeight = fontMetrics.getAscent();
+		return (int)(fontHeight * 1.35);
+	}
+
+
+	/**
+	 * Draws legend texts.
+	 *
+	 * @param graphics The Graphics2D object to draw contents on the graph screen image.
+	 * @param legendTextAreaPosition The array storing x (at [0]) and y (at [1]) of the left-top point of the legend text area (not including markers).
+	 * @param legendTextLineHeight The line-height of the legend texts.
+	 */
+	private void drawLegendTexts(Graphics2D graphics, int[] legendTextAreaPosition, int legendTextLineHeight) {
+
+		// Extract legend settings.
+		String[] legendTexts = this.config.getLabelConfiguration().getLegendLabelConfiguration().getLabelTexts();
+		Font legendFont = this.config.getFontConfiguration().getLegendLabelFont();
+
+		// Extract color settings.
+		Color forgroundColor = this.config.getColorConfiguration().getForegroundColor();
+
+		graphics.setColor(forgroundColor);
+		graphics.setFont(legendFont);
+
 		// Draw legend texts.
 		for (int ilegend=0; ilegend<legendTexts.length; ilegend++) {
 			String legendText = legendTexts[ilegend];
-			int textY = (int)(ilegend * fontHeight * 1.35) + 30;
-			int textX = screenWidth - maxTextWidth - 30;
+			int textX = legendTextAreaPosition[0];
+			int textY = legendTextAreaPosition[1] + (ilegend * legendTextLineHeight);
 			graphics.drawString(legendText, textX, textY);
 		}
 	}
