1c6fbe826f9eea28284f8f7a9792584c50153157
RINEARN <commit@rinearn.com>
2026-07-14 16:14:15 +0900

    Refactor: config.data.Series... classes: Add comments
    Document why these classes are named `Series...` rather than `DataSeries...`

--- src/com/rinearn/graph3d/config/data/SeriesAttribute.java
+++ src/com/rinearn/graph3d/config/data/SeriesAttribute.java
@@ -1,5 +1,19 @@
 package com.rinearn.graph3d.config.data;
 
+//!!!!!
+//Note: This class is named `Series...` rather than `DataSeries...` because
+//the public API treats both data-based plots and formula-based (math) plots uniformly as series.
+//Using DataSeries... for this abstraction could sound unnatural when applied to formula (math) series.
+//
+//The term DataSeries... is commonly used in the internal implementation,
+//where it refers specifically to data-oriented structures.
+//
+//In this externally exposed configuration layer, however,
+//The `Data` prefix is INTENTIONALLY omitted by design.
+//
+//Do not add the Data prefix without careful consideration.
+//Any such change should first be reviewed against the intended abstraction of the public API.
+//!!!!!
 
 /**
  * The container class for storing meta information of a data series.
